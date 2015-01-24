74d83d48d5a2fd2db1512b5ac0ef44c067c36632
Cody Heffner <cody.heffner@gmail.com>
2015-01-24 12:14:07 -0700

    Initialized 453 java directory and project 1.

---
+++ p1/src/textops/TextProcessor.java
@@ -0,0 +1,12 @@
+package textops;
+
+public class TextProcessor {
+
+	public static void main(String[] args) {
+//		1. Tokenize - remove caps, punctuation, hyphens
+//		2. Remove Stopwords
+//		3. Stem remainin words
+//		4. Create indexed structure 
+	}
+
+}
