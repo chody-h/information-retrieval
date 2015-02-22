5df2b01929dae8153eddcfc2920d29593217d84c
Cody Heffner <cody.heffner@gmail.com>
2015-02-21 22:24:13 -0700

    Related query parser complete.

--- p2/src/QuerySuggestor.java
+++ p2/src/QuerySuggestor.java
@@ -10,64 +10,57 @@ public class QuerySuggestor {
 	
 	private PorterStemmer ps = new PorterStemmer();
 	private StopWords sw = new StopWords();
-	private ArrayList<RelatedQueries> rq = new ArrayList<RelatedQueries>();
+	private static ArrayList<RelatedQueries> rq = new ArrayList<RelatedQueries>();
 	
 //	private static Freq_App freq = new Freq_App();
 	private static WCF_App wcf = new WCF_App();
 //	private static Mod_App mod = new Mod_App();
 	
 	public static void main(String[] args) {
+		System.out.println("Parsing files...");
 		ParseFiles();
+		System.out.println("Done parsing files.");
+		PrintSomeQueries();
 	}
 	
 	public static void ParseFiles() {
+		String pre = "files/";
 		String[] docs = {
 				"Clean-Data-01.txt",
-				"Clean-Data-02.txt",
-				"Clean-Data-03.txt",
-				"Clean-Data-04.txt",
-				"Clean-Data-05.txt",
+//				"Clean-Data-02.txt",
+//				"Clean-Data-03.txt",
+//				"Clean-Data-04.txt",
+//				"Clean-Data-05.txt",
 		};
+		int num = 1;
 		for (String doc : docs) {
+			System.out.println("parsing file " + num++);
 			try {
-				BufferedReader in = new BufferedReader(new FileReader("files/Clean-Data-0" + doc_num + ".txt"));
-				StringBuffer str = new StringBuffer();
+				BufferedReader in = new BufferedReader(new FileReader(pre+doc));
 				String nextLine = "";
+				in.readLine();
+				RelatedQueries r = new RelatedQueries();
+				r.AddRelated(nextLine = in.readLine());
 				while ((nextLine = in.readLine()) != null) {
-					str.append(nextLine+"\n");
+//					if (nextLine.split("\\s")[0].equals("AnonID")) continue;
+					if (!r.IsRelated(nextLine)) {
+						rq.add(r);
+						r = new RelatedQueries();
+					}
+					r.AddRelated(nextLine);
 				}
 				in.close();
-
-				String file_contents = str.toString();
-				
-				String[] c = file_contents.split("\\s+");
-				wordcount[doc_num] = c.length;
-				
-				words = Tokenize(file_contents);
 			} catch (IOException e) {
 				e.printStackTrace();
 				return;
 			}
-			
-			for (int i = 0; i < words.length; i++) {
-				
-				if (words[i] == null) continue;
-				
-//					4. Add word to indexed structure 
-				if (index.containsKey(words[i])) {
-					int[] freq = index.get(words[i]);
-					freq[doc_num] += 1;
-					index.put(words[i], freq);
-				}
-				else {
-					int[] freq = new int[NUM_DOCS_TO_PROCESS];
-					freq[doc_num] = 1;
-					index.put(words[i], freq);
-				}
-			}
 		}
-		
-		max_freq_of_doc = MaxFreqOfDoc(-1);
+	}
+	
+	public static void PrintSomeQueries() {
+		for (int i = 0; i < 100; i++) {
+			System.out.println(rq.get(i).toString());
+		}
 	}
 
 }
--- p2/src/RelatedQueries.java
+++ p2/src/RelatedQueries.java
@@ -1,27 +1,68 @@
 import java.util.ArrayList;
+
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 public class RelatedQueries {
 	
 	public int userID;
 	public ArrayList<Query> queries;
 	
-	public RelatedQueries(String line_from_log) {
-		
+	public RelatedQueries() {
+		userID = 0;
+		queries = new ArrayList<Query>();
 	}
 	
 	public boolean IsRelated(String line_from_log) {
-		return false;
+		// false: nothing in it
+		// false: user id is different
+		// false: more than 10min apart
+		// true: otherwise
+		String[] query = ParseLine(line_from_log);
+		try {
+			int uID = Integer.parseInt(query[0]);
+			Query q = new Query(query[1], query[2]);
+			if (uID != userID) return false;
+			if (q.time.getMillis() - queries.get(queries.size() - 1).time.getMillis() > 600000) return false;
+		}
+		catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+	
+	public void AddRelated(String line_from_log) {
+		String[] query = ParseLine(line_from_log);
+		userID = Integer.parseInt(query[0]);
+		Query q = new Query(query[1], query[2]);
+		queries.add(q);
 	}
 	
 	private String[] ParseLine(String line_from_log) {
-		String[] tokens = line_from_log.split(" ");
-		String[] ret = new String[3];
-		ret[0] = tokens[0];														// userID
-		ret[2] = tokens[tokens.length-2] + ' ' + tokens[tokens.length-1];		// datetime
-		ret[1] = line_from_log.replace(ret[0], "");								// query
-		ret[1] = ret[1].replace(ret[2], "");
-		return ret;
+		String[] tokens = line_from_log.split("\\s");
+		try {
+			String[] ret = new String[3];
+			ret[0] = tokens[0];														// userID
+			ret[2] = tokens[tokens.length-2] + ' ' + tokens[tokens.length-1];		// datetime
+			ret[1] = line_from_log.replace(ret[0], "").replace(ret[2], "").replace("\t", "");	// query
+			return ret;
+		}
+		catch (Exception e) {
+//			e.printStackTrace();
+			String[] ret = new String[3];
+			return ret;
+		}
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(userID);
+		sb.append("\n");
+		for (Query q : queries) {
+			sb.append(q.toString());
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 	
 	private class Query {
@@ -30,7 +71,12 @@ public class RelatedQueries {
 		
 		Query(String q, String t) {
 			text = q;
-			time = new DateTime(t);
+//			time = new DateTime(t);
+			time = DateTime.parse(t, DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss"));
+		}
+		
+		public String toString() {
+			return DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").print(time) + ": " + text;
 		}
 	}
 }
